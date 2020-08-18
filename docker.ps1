02414d1aa22aee9e5c51a2d06a251256f24ce7d2
Amit Agrahari <amit.agrahari@nagarro.com>
2020-08-19 00:33:59 +0530

    Change in Jenkins file

--- docker.ps1
+++ docker.ps1
@@ -2,5 +2,6 @@ $containerid = docker ps -f name=devops_application
 if ($containerid)
 {
  	write-host "Container exists | deleting"
- 	docker rm -f $containerid
+ 	docker container stop $containerid
+ 	docker container rm  $containerid
 }
