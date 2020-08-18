4397b9b18b26307be5f8910dcdea5c15fba545af
Amit Agrahari <amit.agrahari@nagarro.com>
2020-08-19 00:44:59 +0530

    Add PowerShell command

--- docker.ps1
+++ docker.ps1
@@ -2,6 +2,5 @@ $containerid = docker ps -f name=devops_application
 if ($containerid)
 {
  	write-host "Container exists | deleting"
- 	docker container stop -f $containerid
  	docker container rm -f $containerid
 }
